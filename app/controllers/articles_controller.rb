e8ab395c95e057c6c77c4242ac7a6e8a2f1aa9e1
Subrat Saxena <subratsaxena9@gmail.com>
2018-08-20 19:15:19 -0400

    Added all the Crud Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+    
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+     flash[:notice] = "Article has been successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def create
    # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -14,8 +28,19 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def index
+   @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+   @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article has been successfully deleted."
+    redirect_to articles_path
   end
   
   private
